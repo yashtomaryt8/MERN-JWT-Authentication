f8794b2cc7ef50ac3f0e693201f48f835a863c81
Yash Tomar <yashtomar.yt8@gmail.com>
2025-10-16 16:27:04 +0530

    Deploy 2

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://mern-jwt-frontend-nine.vercel.app']
 
 // Middlewares
 app.use(express.json())
